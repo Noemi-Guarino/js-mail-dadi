7058cf8ce5d45d9747c265d5a441798db9bf25a8
Noemi-Guarino <noemi.guarino.1996@gmail.com>
2023-12-07 00:18:47 +0100

    edit

--- mail/js/scripts.js
+++ mail/js/scripts.js
@@ -4,15 +4,28 @@
 // -controlla che sia nella lista di chi può accedere,
 // -stampa un messaggio appropriato sull'esito del controllo.
 
- let correctemail = prompt("Inserisci la tua e-mail");
- console.log(correctemail);
- 
- const emailList = [
- ];
+let correctemail = prompt("Inserisci la tua e-mail");
+console.log(correctemail);
 
- console.log(`guarino.noemi96@gmail.com` == values[5]);
+const emailList = [
+    "pincopallo@gmail.com",
+    "cicciogamer@gmail.com",
+    "costanzosenzacostanza@gmail.com",
+    "ginopeppino@gmail.com",
+];
+
+for (let i = 0; i < emailList.length; i++){
+
+    if (correctemail == emailList[i]) { 
+        alert ("la tua e-mail è valida")
+        console.log("corretta");
+    }
+    else {
+        alert ("la tua e-mail non è valida")
+        console.log("non è corretta");
+    }
+}
 
 
 
-// document.getElementById("searchemail").innerHTML =
-// "hai inserito corettamente la tua email:" + email;
+
