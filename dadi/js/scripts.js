66e99514be0bef0568cb7878a8d5b167962cda67
Noemi-Guarino <noemi.guarino.1996@gmail.com>
2023-12-06 18:33:43 +0100

    edit

--- dadi/js/scripts.js
+++ dadi/js/scripts.js
@@ -5,19 +5,30 @@ game.addEventListener ("click",
         console.log(Math.random());
         document.getElementById("resultuser").innerHTML ="Hai pescato il numero:" + resultuser;
 
-        let resultpc = Math.floor(Math.random() * 6 + 1);
+        resultpc = Math.floor(Math.random() * 6 + 1);
         console.log(Math.random());
         document.getElementById("resultpc").innerHTML ="Il tuo avversario ha pescato il numero:" + resultpc ;
 
-        if (resultuser > resultpc ) {
-            document.getElementById("winner").innerHTML = "HAI VINTO!" 
-        }
+        
 
-        else if (resultuser === resultpc ) {
-        document.getElementById("winner").innerHTML = "RITENTA,C'É STATO UN PAREGGIO";
-        }
+            if (resultuser > resultpc ) {
+                console.log("vincitore");
+                result = "HAI VINTO";
+                document.getElementById("winner").innerHTML = "HAI VINTO!" 
 
-        else (resultuser < resultpc ) ;
-        document.getElementById("winner").innerHTML = "RITENTA,SARAI PIÙ FORTUNATO/A";                
-    }
-)
+            } else if (resultuser == resultpc ) {
+                console.log("pareggio");
+                result = "HAI PAREGGIATO";
+                document.getElementById("winner").innerHTML = "RITENTA,C'É STATO UN PAREGGIO";
+
+            } else {
+                console.log("perdente");
+                result = "HAI PERSO";
+                document.getElementById("winner").innerHTML = "RITENTA,SARAI PIÙ FORTUNATO/A"; 
+            }
+
+        }       
+    )
+
+
+document.getElementById("winner").innerHTML = result ;
